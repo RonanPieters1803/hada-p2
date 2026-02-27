5eab40808c351bbe1d76d0299aeac2d60883590f
Ronan <ronanpieter1803@gmail.com>
2026-02-27 12:59:00 +0100

    implementacion barco.cs y6955399y

--- hada-p2/Barco.cs
+++ hada-p2/Barco.cs
@@ -1,12 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Hada
 {
+    internal class CoordenadaEventArgs : EventArgs
+    {
+        public Coordenada Coordenada { get; private set; }
+
+        public CoordenadaEventArgs(Coordenada coordenada)
+        {
+            Coordenada = coordenada;
+        }
+    }
     internal class Barco
     {
+        public event EventHandler<CoordenadaEventArgs> Tocado;
+        public event EventHandler Hundido; 
+
+        public Dictionary<Coordenada, String> CoordenadasBarco {  get; private set; }
+        public string Nombre { get; private set; }
+        public int  NumDanyos { get; private set; }
+
+        public Barco(string nombre, int longitud, char orientacion, Coordenada coordenadaInicio) 
+        {
+            Nombre = nombre;
+            NumDanyos = 0;
+            CoordenadasBarco = new Dictionary<Coordenada, String>(); 
+            for (int i = 0; i < longitud; i++)
+            {
+                Coordenada coor;
+                if (orientacion == 'v')
+                {
+                    coor = new Coordenada(coordenadaInicio.Fila+1, coordenadaInicio.Columna);
+                    
+                }
+                else
+                {
+                    coor = new Coordenada(coordenadaInicio.Fila, coordenadaInicio.Columna + i);
+                }
+                CoordenadasBarco[coor] = nombre;
+            }
+        }
+
+        public void Disparo(Coordenada c)
+        {
+            if (CoordenadasBarco.ContainsKey(c))
+            {
+                if (!CoordenadasBarco[c].EndsWith("_T"))
+                {
+                    CoordenadasBarco[c] += "_T";
+                    Tocado?.Invoke(this, new CoordenadaEventArgs(c));
+                    NumDanyos++;
+                    if (hundido())
+                    {
+                        Hundido?.Invoke(this, EventArgs.Empty);
+                    }
+                }
+            }
+        }
+
+        public bool hundido()
+        {
+            foreach (string etiqueta in CoordenadasBarco.Values)
+            {
+                if (etiqueta == Nombre) 
+                    return false;
+            }
+            return true;
+        }
     }
 }
