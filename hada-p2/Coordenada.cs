1ca1e6cee0fd37fa0a3bf3271a91ebbb45779c91
Ronan <ronanpieter1803@gmail.com>
2026-02-27 11:28:19 +0100

    Implementar coordenadas.cs y6955399y

--- hada-p2/Coordenada.cs
+++ hada-p2/Coordenada.cs
@@ -8,5 +8,39 @@ namespace Hada
 {
     internal class Coordenada
     {
+        private int _fila, _columna;
+
+        public int Fila
+        {
+            get { return _fila; }
+
+            set
+            {
+                if (_fila > 0 && _fila < 9)
+                {
+                    _fila = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Fila entre 0 y 9");
+                }
+            }
+        }
+        public int Columna
+        {
+            get { return _columna; }
+
+            set
+            {
+                if (_columna > 0 && _columna < 9)
+                {
+                    _columna = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Columna entre 0 y 9");
+                }
+            }
+        }
     }
 }
